cb8b04bfeee32926c17d10ed9c6969f87d8a3ec3
Schparky <3172830+Schparky@users.noreply.github.com>
2021-12-13 22:05:23 -0700

    implement markbates/goth for authentication

--- application/go.mod
+++ application/go.mod
@@ -5,11 +5,18 @@ go 1.17
 require (
 	github.com/labstack/echo/v4 v4.6.1
 	github.com/lib/pq v1.10.3
+	github.com/markbates/goth v1.68.0
 	github.com/pressly/goose/v3 v3.4.1
 )
 
 require (
+	cloud.google.com/go v0.67.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.1.1 // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -18,7 +25,10 @@ require (
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43 // indirect
 	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- application/server.go
+++ application/server.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
-
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/markbates/goth"
+	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/auth0"
+	"github.com/markbates/goth/providers/google"
+	"html/template"
 	"net/http"
+	"os"
 )
+
+type ProviderIndex struct {
+	Providers    []string
+	ProvidersMap map[string]string
+}
+
+var indexTemplate = `{{range $key,$value:=.Providers}}
+    <p><a href="/auth/login?provider={{$value}}">Log in with {{index $.ProvidersMap $value}}</a></p>
+{{end}}`
+
+var userTemplate = `
+<p><a href="/logout/{{.Provider}}">logout</a></p>
+<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
+<p>Email: {{.Email}}</p>
+<p>NickName: {{.NickName}}</p>
+<p>Location: {{.Location}}</p>
+<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
+<p>Description: {{.Description}}</p>
+<p>UserID: {{.UserID}}</p>
+<p>AccessToken: {{.AccessToken}}</p>
+<p>ExpiresAt: {{.ExpiresAt}}</p>
+<p>RefreshToken: {{.RefreshToken}}</p>
+`
 
 func main() {
 	// Echo instance
@@ -15,11 +44,57 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
+	// Goth
+	goth.UseProviders(
+		auth0.New(os.Getenv("AUTH0_KEY"), os.Getenv("AUTH0_SECRET"), "http://localhost:3000/auth/auth0/callback", os.Getenv("AUTH0_DOMAIN")),
+		google.New(os.Getenv("GOOGLE_KEY"), os.Getenv("GOOGLE_SECRET"), "http://localhost:3000/auth/google/callback"),
+	)
+
 	// Route => handler
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!\n")
-	})
+	e.GET("/", home)
+	e.GET("/auth/login", authLogin)
+	e.GET("/auth/callback", authCallback)
+	e.GET("/logout", authLogout)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func home(c echo.Context) error {
+	m := map[string]string{"auth0": "Auth0", "google": "Google"}
+	keys := []string{"auth0", "google"}
+	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
+
+	t, _ := template.New("foo").Parse(indexTemplate)
+	t.Execute(c.Response(), providerIndex)
+	return nil
+}
+
+func authLogin(c echo.Context) error {
+	//try to get the user without re-authenticating
+	if gothUser, err := gothic.CompleteUserAuth(c.Response(), c.Request()); err == nil {
+		t, _ := template.New("foo").Parse(userTemplate)
+		t.Execute(c.Response(), gothUser)
+	} else {
+		gothic.BeginAuthHandler(c.Response(), c.Request())
+	}
+
+	return nil
+}
+
+func authLogout(c echo.Context) error {
+	gothic.Logout(c.Response(), c.Request())
+	c.Response().Header().Set("Location", "/")
+	c.Response().WriteHeader(http.StatusTemporaryRedirect)
+	return nil
+}
+
+func authCallback(c echo.Context) error {
+	user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
+	if err != nil {
+		_, err = fmt.Fprintln(c.Response(), err)
+		return err
+	}
+	t, _ := template.New("foo").Parse(userTemplate)
+	return t.Execute(c.Response(), user)
 }
