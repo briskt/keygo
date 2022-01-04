84db384dbe1162b64a2722e74c44b8eb24b2bf91
Schparky <3172830+Schparky@users.noreply.github.com>
2022-01-03 19:59:20 -0700

    refactor auth, combine init and authLogin provider lists

--- application/http/auth.go
+++ application/http/auth.go
@@ -1,7 +1,6 @@
 package http
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 
@@ -24,69 +23,83 @@ type ProviderOption struct {
 	RedirectURL string
 }
 
+type providerConfig struct {
+	key         string
+	name        string
+	authKey     string
+	secret      string
+	callbackURL string
+	domain      string
+}
+
+var providers = []providerConfig{
+	{
+		key:         "auth0",
+		name:        "Auth0",
+		authKey:     os.Getenv("AUTH0_KEY"),
+		secret:      os.Getenv("AUTH0_SECRET"),
+		callbackURL: os.Getenv("HOST") + AuthCallbackPath + "?provider=auth0",
+		domain:      os.Getenv("AUTH0_DOMAIN"),
+	}, {
+		key:         "google",
+		name:        "Google",
+		authKey:     os.Getenv("GOOGLE_KEY"),
+		secret:      os.Getenv("GOOGLE_SECRET"),
+		callbackURL: os.Getenv("HOST") + AuthCallbackPath + "?provider=google",
+	},
+}
+
 func init() {
-	// Goth
-	goth.UseProviders(
-		auth0.New(os.Getenv("AUTH0_KEY"),
-			os.Getenv("AUTH0_SECRET"),
-			os.Getenv("HOST")+AuthCallbackPath+"?provider=auth0",
-			os.Getenv("AUTH0_DOMAIN"),
-		),
-		google.New(
-			os.Getenv("GOOGLE_KEY"),
-			os.Getenv("GOOGLE_SECRET"),
-			os.Getenv("HOST")+AuthCallbackPath+"?provider=google",
-		),
-	)
+	for _, p := range providers {
+		if p.secret == "" || p.authKey == "" {
+			continue
+		}
+		switch p.key {
+		case "auth0":
+			goth.UseProviders(auth0.New(p.authKey, p.secret, p.callbackURL, p.domain))
+		case "google":
+			goth.UseProviders(google.New(p.authKey, p.secret, p.callbackURL))
+		}
+	}
 }
 
 func RegisterAuthRoutes(e *echo.Echo) {
-	// Route => handler
-	e.POST("/auth/login", authLogin)
+	e.GET("/auth/login", authLogin)
 	e.GET(AuthCallbackPath, authCallback)
 	e.GET("/auth/logout", authLogout)
 }
 
 func authLogin(c echo.Context) error {
-	res := c.Response()
-	req := c.Request()
-
-	// try to get the user without re-authenticating
-	if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
-		return c.JSON(http.StatusOK, gothUser)
-	}
-
-	fmt.Printf("client ID: %s\n", c.QueryParam("client_id"))
-	allProviders := map[string]string{"auth0": "Auth0", "google": "Google"}
-	options := make([]ProviderOption, len(allProviders))
-	i := 0
-	for key, name := range allProviders {
-		options[i] = ProviderOption{Key: key, Name: name}
-
-		gothic.GetProviderName = func(req *http.Request) (string, error) {
-			return key, nil
+	options := make([]ProviderOption, 0)
+	for _, p := range providers {
+		if p.secret == "" || p.authKey == "" {
+			continue
 		}
-		url, err := gothic.GetAuthURL(res, req)
+		gothic.GetProviderName = func(req *http.Request) (string, error) { return p.key, nil }
+		url, err := gothic.GetAuthURL(c.Response(), c.Request())
 		if err != nil {
-			return c.JSON(http.StatusBadRequest, AuthError{Error: err.Error()})
+			return c.JSON(http.StatusInternalServerError, AuthError{Error: err.Error()})
 		}
-		options[i].RedirectURL = url
-		i++
+		options = append(options, ProviderOption{
+			Key:         p.key,
+			Name:        p.name,
+			RedirectURL: url,
+		})
 	}
 	return c.JSON(http.StatusOK, options)
 }
 
 func authLogout(c echo.Context) error {
-	gothic.Logout(c.Response(), c.Request())
-	c.Response().Header().Set("Location", "/")
-	c.Response().WriteHeader(http.StatusTemporaryRedirect)
-	return nil
+	err := gothic.Logout(c.Response(), c.Request())
+	if err != nil {
+		return err
+	}
+	return c.Redirect(http.StatusTemporaryRedirect, os.Getenv("UI_URL"))
 }
 
 func authCallback(c echo.Context) error {
 	user, err := gothic.CompleteUserAuth(c.Response(), c.Request())
 	if err != nil {
-		_, err = fmt.Fprintln(c.Response(), err)
 		return err
 	}
 	return c.JSON(http.StatusOK, user)
