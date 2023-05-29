8ff311abd84dc3a9c755d6f206899a70b1ef66bd
briskt <3172830+briskt@users.noreply.github.com>
2023-05-29 20:50:31 +0800

    move definition above use

--- application/assets/data/store/auth.ts
+++ application/assets/data/store/auth.ts
@@ -4,10 +4,9 @@ const clientIDparam = 'clientID'
 export const getToken = () => getClientID() + (localStorage.getItem(tokenParam) || '')
 export const getClientID = () => localStorage.getItem(clientIDparam)
 
+const makeRandomID = () => Math.random().toString(36).slice(2)
+
 init()
 function init() {
     localStorage.getItem(clientIDparam) || localStorage.setItem(clientIDparam, makeRandomID())
 }
-
-const makeRandomID = () => Math.random().toString(36).slice(2)
-
