ec71df7da26f44b7a07c96fe9521b1f2d6e804b2
briskt <3172830+briskt@users.noreply.github.com>
2023-11-25 22:09:26 -0700

    test tenantsCreateHandler

--- server/tenant_test.go
+++ server/tenant_test.go
@@ -1,6 +1,7 @@
 package server_test
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -9,7 +10,37 @@ import (
 	"github.com/labstack/echo/v4"
 
 	"github.com/briskt/keygo/app"
+	"github.com/briskt/keygo/db"
 )
+
+func (ts *TestSuite) Test_tenantsCreateHandler() {
+	f := ts.createUserFixture()
+	token := f.Tokens[0]
+
+	input := app.TenantCreateInput{Name: "new tenant"}
+	j, _ := json.Marshal(&input)
+	req := httptest.NewRequest(http.MethodPost, "/api/tenants", bytes.NewReader(j))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	req.Header.Set(echo.HeaderAuthorization, "Bearer "+token.PlainText)
+
+	res := httptest.NewRecorder()
+	ts.server.ServeHTTP(res, req)
+	body, err := io.ReadAll(res.Body)
+	ts.NoError(err)
+
+	// Assertions
+	ts.Equal(http.StatusOK, res.Code, "incorrect http status, body: \n%s", body)
+
+	var gotTenant app.Tenant
+	ts.NoError(json.Unmarshal(body, &gotTenant))
+	ts.Equal(input.Name, gotTenant.Name, "incorrect Tenant Name, body: \n%s", body)
+
+	dbTenant, err := db.FindTenantByID(ts.ctx, gotTenant.ID)
+	ts.NoError(err)
+	ts.Equal(input.Name, dbTenant.Name, "incorrect Tenant Name in db")
+
+	// TODO: test error response
+}
 
 func (ts *TestSuite) Test_GetTenant() {
 	f := ts.createUserFixture()
