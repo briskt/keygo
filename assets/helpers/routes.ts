32002f974077089fc4a389e35dc462ce2ea103b6
briskt <3172830+briskt@users.noreply.github.com>
2023-12-02 20:24:57 -0700

    remove /exit page

--- assets/helpers/routes.ts
+++ assets/helpers/routes.ts
@@ -7,7 +7,6 @@ export const USERS = '/admin/users'
 
 export const PROFILE = '/user'
 export const LOGOUT = '/logout'
-export const EXIT = '/exit'
 
 export const TERMS_OF_USE = '/terms'
 export const PRIVACY_POLICY = '/privacy'
