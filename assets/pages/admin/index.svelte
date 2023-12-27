abd464b064cd91a7111debed82c7016039c0b54d
briskt <3172830+briskt@users.noreply.github.com>
2023-12-27 16:52:38 -0700

    show number of tenants on admin page

--- assets/pages/admin/index.svelte
+++ assets/pages/admin/index.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import {listTenants} from 'data/api/tenants'
   import {listUsers} from 'data/api/users'
   import {onMount} from 'svelte'
 
+  let tenantCount
   let userCount
 
   onMount(async () => {
+    tenantCount = (await listTenants()).length
     userCount = (await listUsers()).length
   })
 </script>
 
 <h2>Home</h2>
 
-Total number of users: {userCount || ''}
+Total number of tenants: {tenantCount || ''} <br>
+Total number of users: {userCount || ''} <br>
